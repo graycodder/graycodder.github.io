bfac33e13daea44660d977353f6b81793da3e0ea
Aseem N <graycodder>
2013-06-23 19:53:35 +0530

    desing fix

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-	jQuery(document).ready(function(){
+	jQuery(document).ready(function(e){
 		
 		var pathname = window.location.href;
 					var pathname =  pathname.split(".html")[1] 
@@ -6,15 +6,22 @@
 					scrollTop :($(pathname).offset().top - 80) 
 					}, 800);
 		
-		$('.nav li a').click(function(){
+		$('.nav li a').click(function(e){
+			
+			
+		   
 			
 			var idFind = $(this).attr('href').split("#")[1]
 			var idFind = '#'+idFind
 			if(idFind!='#')
-			$('html, body').animate({
+			{$('html, body').animate({
 				scrollTop :($(idFind).offset().top - 80) 
-			}, 800);
-		});
+			}, 800);}else{
+				$('body,html').animate({
+								scrollTop: 0
+							}, 800);
+			}
+		    	});
 		
 		$('#contact-back ,#about-back' ).click(function(){
 			
