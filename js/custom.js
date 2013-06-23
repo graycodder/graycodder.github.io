001add3a2211455f7dee3cbf1d75627521ceb8cc
Aseem N <graycodder>
2013-06-23 17:10:48 +0530

    fonts

--- js/custom.js
+++ js/custom.js
@@ -4,18 +4,24 @@
 					var pathname =  pathname.split(".html")[1] 
 		  			$('html, body').animate({
 					scrollTop :($(pathname).offset().top - 80) 
-					});
+					}, 800);
 		
 		$('.nav li a').click(function(){
-			console.log("sdfsfds");
+			
 			var idFind = $(this).attr('href').split("#")[1]
 			var idFind = '#'+idFind
 			if(idFind!='#')
 			$('html, body').animate({
 				scrollTop :($(idFind).offset().top - 80) 
-			});
+			}, 800);
 		});
 		
+		$('#contact-back ,#about-back' ).click(function(){
+			
+			$('body,html').animate({
+							scrollTop: 0
+						}, 800);
+		});
 		
 	
 		
