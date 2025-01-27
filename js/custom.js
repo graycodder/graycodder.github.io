6da2ca494e131212b7e421c834f1590f6cfca60b
Alwin Antony <alwin1658@gmail.com>
2025-01-27 15:42:23 +0530

    Added Cookies

--- js/custom.js
+++ js/custom.js
@@ -419,3 +419,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 // This is for Portfolio works end //
+// Cookies Section Start//
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+// Cookies Section End//
